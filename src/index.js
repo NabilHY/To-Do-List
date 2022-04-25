675b2941473ea6b2cebbdce64d4682bbf5e25f99
nabsternab <hayounn.etd@gmail.com>
2022-04-25 23:02:21 +0000

    Add Styling and Edit itteration Fucntion

--- src/index.js
+++ src/index.js
@@ -25,8 +25,18 @@ console.log(tasks);
 
 const itterate = () => {
   tasks.forEach((task) => {
-    const newTask = document.createElement('p');
-    newTask.innerText = `${task.description}`;
+    const newTask = document.createElement('div');
+    newTask.classList.add('task-row');
+    const rmvIcon = document.createElement('i');
+    rmvIcon.classList.add('fa-regular');
+    rmvIcon.classList.add('fa-square-full');
+
+    const taskText = document.createElement('p');
+    taskText.innerHTML = `${task.description}`;
+    const editIcon = document.createElement('i');
+    editIcon.classList.add('fa-regular');
+    editIcon.classList.add('fa-pen-to-square');
+    newTask.append(rmvIcon, taskText, editIcon);
     tasksSection.appendChild(newTask);
   })
 }
