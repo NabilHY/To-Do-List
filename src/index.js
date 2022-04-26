a7d7bdd513084eda57dee5be6126cd0ae87a8ece
nabsternab <hayounn.etd@gmail.com>
2022-04-26 13:38:06 +0000

    Implement Adding function

--- src/index.js
+++ src/index.js
@@ -1,44 +1,7 @@
 import './style.css';
+import { loadLS, addTask } from '../newFunc.js';
 
-const tasksSection = document.querySelector('.task-section');
+const addBtn = document.getElementById('add-btn');
 
-const tasks = [
-  {
-    description: 'Clean Room',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Go Shopping',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Walk Dog',
-    completed: false,
-    index: 3,
-  },
-];
-
-const itterate = () => {
-  tasks.forEach((task) => {
-    const newTask = document.createElement('div');
-    newTask.classList.add('task-row');
-    const rmvIcon = document.createElement('i');
-    rmvIcon.classList.add('fa-regular');
-    rmvIcon.classList.add('fa-square-full');
-    const div = document.createElement('div');
-    const taskText = document.createElement('p');
-    taskText.innerHTML = `${task.description}`;
-    const square = document.createElement('i');
-    square.classList.add('fa-regular');
-    square.classList.add('fa-pen-to-square');
-    div.append(square, taskText);
-    newTask.append(div, square);
-    tasksSection.appendChild(newTask);
-  });
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-  itterate();
-});
+addBtn.addEventListener('click', addTask);
+document.addEventListener('DOMContentLoaded', loadLS);
